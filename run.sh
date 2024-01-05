5c55efe99de5e39178801a7da027e87f4407ddac
jlebre <jlebre@Inception.myguest.virtualbox.org>
2024-01-05 03:56:26 +0000

    Moving to Wordpress

--- run.sh
+++ run.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-apt-get update
-apt-get install -y python3-pip
-apt-get install -y git
-apt-get install -y docker.io
-apt-get install -y docker-compose
+sudo apt-get update
+sudo apt-get install -y python3-pip
+sudo apt-get install -y git
+sudo apt-get install -y docker.io
+sudo apt-get install -y docker-compose
+sudo apt-get install -y nginx
 
 git clone https://www.github.com/jlebre/42Inception.git
 cd 42Inception
