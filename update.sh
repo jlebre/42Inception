dddf90fed736dbd861f87d4f850a201efa837420
jlebre <josemariamlebre@gmail.com>
2024-02-28 19:56:47 +0000

    Changes to Makefile e to update

--- update.sh
+++ update.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+# This script is used to update the git repository
+
+# Add all files to the repository
 git add .
-git commit -m "update"
+
+# Commit the changes
+# If a commit message is provided, use it
+# Otherwise, use a default message
+if (( $# == 1 )); then
+    git commit -m "$1"
+else
+    git commit -m "update"
+fi
+
+# Push the changes to the remote repository
 git push
