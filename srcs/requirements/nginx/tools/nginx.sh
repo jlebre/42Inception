d9e10472421355697bd99f0d1608af0bd8297492
jlebre <josemariamlebre@gmail.com>
2024-02-29 18:25:51 +0000

    update

--- srcs/requirements/nginx/tools/nginx.sh
+++ srcs/requirements/nginx/tools/nginx.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-sudo mkdir -p ~/data/
-sudo mkdir -p /etc/nginx/ssl/
+mkdir -p ~/data/
+mkdir -p /etc/nginx/ssl/
 
-openssl genpkey -algorithm RSA -out /etc/nginx/ssl/server.key 
-openssl req -new -key /etc/nginx/ssl/server.key -out /etc/nginx/ssl/server.csr -subj "/C=PT/ST=LS/L=LS/O=42/OU=student/CN=${LOGIN}"
-openssl x509 -req -in /etc/nginx/ssl/server.csr -signkey /etc/nginx/ssl/server.key -out /etc/nginx/ssl/server.crt
+#openssl genpkey -algorithm RSA -out /etc/nginx/ssl/server.key 
+#openssl req -new -key /etc/nginx/ssl/server.key -out /etc/nginx/ssl/server.csr -subj "/C=PT/ST=LS/L=LS/O=42/OU=student/CN=${LOGIN}"
+#openssl x509 -req -in /etc/nginx/ssl/server.csr -signkey /etc/nginx/ssl/server.key -out /etc/nginx/ssl/server.crt
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/nginx/ssl/server.key -out /etc/nginx/ssl/server.crt
+
+chmod 600 /etc/nginx/ssl/server.key
 
 nginx -g "daemon off;"
