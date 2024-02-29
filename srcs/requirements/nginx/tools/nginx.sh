bf17552ffdd21a143e76939ff97effe80f508fc1
jlebre <josemariamlebre@gmail.com>
2024-02-29 17:37:09 +0000

    update

--- srcs/requirements/nginx/tools/nginx.sh
+++ srcs/requirements/nginx/tools/nginx.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
 mkdir -p ~/data/
+mkdir -p /etc/nginx/ssl/
+mkdir -p /etc/nginx/conf.d/
+
+# Give permissions
+chmod 644 /etc/nginx/nginx.conf
+
+# Copy nginx configuration
+cp ../conf/nginx.conf /etc/nginx/nginx.conf
+cp ../conf/nginx.conf /etc/nginx/conf.d/nginx.conf
 
 openssl genpkey -algorithm RSA -out /etc/nginx/ssl/server.key 
 openssl req -new -key /etc/nginx/ssl/server.key -out /etc/nginx/ssl/server.csr -subj "/C=PT/ST=LS/L=LS/O=42/OU=student/CN=${LOGIN}"
