3ef0c25c0c49036626207dd926e6e013d775a889
jlebre <josemariamlebre@gmail.com>
2024-02-29 17:17:01 +0000

    update

--- srcs/requirements/nginx/tools/nginx.sh
+++ srcs/requirements/nginx/tools/nginx.sh
@@ -3,7 +3,7 @@
 mkdir -p ~/data/
 
 openssl genpkey -algorithm RSA -out /etc/nginx/ssl/server.key 
-openssl req -new -key /etc/nginx/ssl/server.key -out /etc/nginx/ssl/server.csr -subj "/C=PT/ST=LS/L=LS/O=42/OU=student/CN=jlebre"
+openssl req -new -key /etc/nginx/ssl/server.key -out /etc/nginx/ssl/server.csr -subj "/C=PT/ST=LS/L=LS/O=42/OU=student/CN=${LOGIN}"
 openssl x509 -req -in /etc/nginx/ssl/server.csr -signkey /etc/nginx/ssl/server.key -out /etc/nginx/ssl/server.crt
 
 nginx -g "daemon off;"
