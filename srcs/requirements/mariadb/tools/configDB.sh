096523ce874b33121c2da56e6fda6649014ee0b0
jlebre <josemariamlebre@gmail.com>
2024-03-02 20:17:42 +0000

    update

--- srcs/requirements/mariadb/tools/configDB.sh
+++ srcs/requirements/mariadb/tools/configDB.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Start the MariaDB service
-sudo service mariadb start
+service mariadb start
 
 echo "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" | mariadb
 echo "GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" | mariadb
@@ -10,9 +10,9 @@ echo "FLUSH PRIVILEGES;" | mariadb
 
 echo "CREATE DATABASE $MYSQL_DATABASE;" | mariadb
 
-sudo service mariadb stop
+service mariadb stop
 
 echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';" | mariadb -u root -p $MYSQL_ROOT_PASSWORD --socket=/run/mysqld/mysqld.sock
 echo "FLUSH PRIVILEGES;" | mariadb -u root -p $MYSQL_ROOT_PASSWORD --socket=/run/mysqld/mysqld.sock
 
-sudo exec mysqld --socket=/run/mysqld/mysqld.sock --pid-file=/run/mysqld/mysqld.pid
+exec mysqld --socket=/run/mysqld/mysqld.sock --pid-file=/run/mysqld/mysqld.pid
