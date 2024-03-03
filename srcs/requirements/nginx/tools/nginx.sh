546c50888aa6b5476ee67056c8b62077433f087a
jlebre <josemariamlebre@gmail.com>
2024-03-03 07:49:55 +0000

    Wordpress failing

--- srcs/requirements/nginx/tools/nginx.sh
+++ srcs/requirements/nginx/tools/nginx.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
 if [ ! -f /etc/ssl/certs/nginx.crt ]; then
-    openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/nginx/ssl/server.key -out /etc/nginx/ssl/server.crt
-fi
-if [ $? -ne 0 ]; then
-    echo "Failed to generate SSL certificate and key"
-    exit 1
+    openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/certs/nginx.key -out /etc/ssl/certs/nginx.crt  -subj "/C=PT/ST=Lisbon/L=Lisbon/O=42Lisboa/CN=jlebre.42.fr";
 fi
 
 exec "$@"
