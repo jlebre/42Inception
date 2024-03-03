49cd8b64f1460d92bb87f83e331938a57242ec75
jlebre <josemariamlebre@gmail.com>
2024-03-03 07:53:52 +0000

    Wordpress failing

--- srcs/requirements/nginx/tools/nginx.sh
+++ srcs/requirements/nginx/tools/nginx.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ ! -f /etc/ssl/certs/nginx.crt ]; then
-    openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/certs/nginx.key -out /etc/ssl/certs/nginx.crt  -subj "/C=PT/ST=Lisbon/L=Lisbon/O=42Lisboa/CN=jlebre.42.fr";
+    openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx.key -out /etc/ssl/certs/nginx.crt  -subj "/C=PT/ST=Lisbon/L=Lisbon/O=42Lisboa/CN=jlebre.42.fr";
 fi
 
 exec "$@"
