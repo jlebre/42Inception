b05ee3522ddcf65cee62e5cb9d6c66518d8774fe
jlebre <josemariamlebre@gmail.com>
2024-03-03 23:20:05 +0000

    update

--- srcs/requirements/mariadb/tools/DB.sql
+++ srcs/requirements/mariadb/tools/DB.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE IF NOT EXISTS wordpress;
-CREATE USER IF NOT EXISTS 'jlebre'@'%' IDENTIFIED BY 'jlebre123';
-GRANT ALL PRIVILEGES ON wordpress.* TO 'jlebre'@'%';
+CREATE USER IF NOT EXISTS 'sql_user'@'%' IDENTIFIED BY 'sql_pass';
+GRANT ALL PRIVILEGES ON wordpress.* TO 'sql_user'@'%';
 FLUSH PRIVILEGES;
 
 ALTER USER 'root'@'localhost' IDENTIFIED BY 'root123';
