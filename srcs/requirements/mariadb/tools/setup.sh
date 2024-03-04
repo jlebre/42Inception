852d4cdad80326ac7745fab68bbb037f776fc0f1
jlebre <josemariamlebre@gmail.com>
2024-03-04 22:40:55 +0000

    update

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -7,14 +7,15 @@ fi
 
 if [ ! -d /var/lib/mysql/mysql ]; then
     mysql_install_db --user=mysql --datadir=/var/lib/mysql
-    service mariadb start
+    service mysql start
 
     mysql -u $MYSQL_ROOT -p"$MYSQL_ROOT_PASSWORD" "CREATE DATABASE IF NOT EXISTS $DATABASE_NAME;"
     mysql -u $MYSQL_ROOT -p"$MYSQL_ROOT_PASSWORD" "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
     mysql -u $MYSQL_ROOT -p"$MYSQL_ROOT_PASSWORD" "GRANT ALL PRIVILEGES ON $DATABASE_NAME.* TO '$MYSQL_USER'@'%';"
     mysql -u $MYSQL_ROOT -p"$MYSQL_ROOT_PASSWORD" "FLUSH PRIVILEGES;"
     mysql -u $MYSQL_ROOT -p"$MYSQL_ROOT_PASSWORD" "ALTER USER '$MYSQL_ROOT'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'"
-    service mariadb stop
+    service mysql stop
+    kill $(cat /var/run/mysqld/mysqld.pid)
 fi
 
 mysqld
