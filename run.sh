4a23938a923f17ea390a05106d337c06fa58ee14
jlebre <josemariamlebre@gmail.com>
2024-03-04 21:16:16 +0000

    update

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-apt-get update && apt-get upgrade -y vim \
-  curl wget make git docker.io docker-compose \
+apt-get update && apt-get upgrade
+apt-get install -y vim curl wget make git docker.io docker-compose \
   mariadb-server mariadb-client \
   nginx openssl php7.3 php-mysqli php-fpm wget sendmail
 
