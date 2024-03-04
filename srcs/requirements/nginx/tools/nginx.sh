73151e308badd81595b9d47c51595d3739d29548
jlebre <josemariamlebre@gmail.com>
2024-03-04 22:38:32 +0000

    update

--- srcs/requirements/nginx/tools/nginx.sh
+++ srcs/requirements/nginx/tools/nginx.sh
@@ -4,7 +4,7 @@ if [ ! -f /etc/ssl/certs/nginx.crt ]; then
     openssl req -x509 -nodes -days 365 -newkey rsa:4096 -keyout /etc/ssl/private/nginx.key -out /etc/ssl/certs/nginx.crt  -subj "/C=PT/ST=Lisbon/L=Lisbon/O=42Lisboa/CN=jlebre.42.fr";
 fi
 
-exec "$@"
+nginx -g "daemon off;"
 
 #───────────────────────────────────────────────────────────────────────#
 # The script creates a self-signed SSL certificate if it doesn't exist. #
