3b993ed09769629679889240360bd864130451d5
jlebre <josemariamlebre@gmail.com>
2024-03-04 21:34:25 +0000

    update

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -1,8 +1,12 @@
-echo "CREATE DATABASE IF NOT EXISTS \${DATABASE_NAME};" > DB.sql
-echo "CREATE USER IF NOT EXISTS '\${MYSQL_USER}'@'%' IDENTIFIED BY '\${MYSQL_PASSWORD}';" >> DB.sql
-echo "GRANT ALL PRIVILEGES ON \${DATABASE_NAME}.* TO '\${MYSQL_USER}'@'%';" >> DB.sql
-echo "FLUSH PRIVILEGES;" >> DB.sql
-echo >> DB.sql
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '\${MYSQL_ROOT_PASSWORD}';" >> DB.sql
+sed -i "s/DATABASE_NAME/$DATABASE_NAME/g" DB.sql
+sed -i "s/\MYSQL_USER/$MYSQL_USER/g" DB.sql
+sed -i "s/\MYSQL_PASSWORD/$MYSQL_PASSWORD/g" DB.sql
+sed -i "s/\MYSQL_ROOT_PASSWORD/$MYSQL_ROOT_PASSWORD/g" DB.sql
 
 exec "$@"
+#───────────────────────────────────────────────────────────────────────#
+# The script replaces the variables in the DB.sql file with the         #
+# values of the environment variables MYSQL_USER, MYSQL_PASSWORD,       #
+# and MYSQL_ROOT_PASSWORD. The script then executes any command-line    #
+# arguments passed to it.                                               #
+#_______________________________________________________________________#
