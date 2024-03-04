fd0fcb846a5f90fc3a17f4be7596e24942b8eb09
jlebre <josemariamlebre@gmail.com>
2024-03-04 22:14:56 +0000

    update

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-service mariadb start
+if [ ! -d /run/mysqld ]; then
+    mkdir -p /run/mysqld
+    chopwn mysql:mysql /run/mysqld
+fi
 
-mysql -u $MYSQL_ROOT -p"$MYSQL_ROOT_PASSWORD" "CREATE DATABASE IF NOT EXISTS $DATABASE_NAME;"
-mysql -u $MYSQL_ROOT -p"$MYSQL_ROOT_PASSWORD" "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
-mysql -u $MYSQL_ROOT -p"$MYSQL_ROOT_PASSWORD" "GRANT ALL PRIVILEGES ON $DATABASE_NAME.* TO '$MYSQL_USER'@'%';"
-mysql -u $MYSQL_ROOT -p"$MYSQL_ROOT_PASSWORD" "FLUSH PRIVILEGES;"
-mysql -u $MYSQL_ROOT -p"$MYSQL_ROOT_PASSWORD" "ALTER USER '$MYSQL_ROOT'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'"
+if [ ! -d /var/lib/mysql/mysql ]; then
+    mysql_install_db --user=mysql --datadir=/var/lib/mysql
+    service mariadb start
 
-service mariadb stop
+    mysql -u $MYSQL_ROOT -p"$MYSQL_ROOT_PASSWORD" "CREATE DATABASE IF NOT EXISTS $DATABASE_NAME;"
+    mysql -u $MYSQL_ROOT -p"$MYSQL_ROOT_PASSWORD" "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
+    mysql -u $MYSQL_ROOT -p"$MYSQL_ROOT_PASSWORD" "GRANT ALL PRIVILEGES ON $DATABASE_NAME.* TO '$MYSQL_USER'@'%';"
+    mysql -u $MYSQL_ROOT -p"$MYSQL_ROOT_PASSWORD" "FLUSH PRIVILEGES;"
+    mysql -u $MYSQL_ROOT -p"$MYSQL_ROOT_PASSWORD" "ALTER USER '$MYSQL_ROOT'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'"
+    service mariadb stop
+fi
 
-exec mysqld --socket=/run/mysqld/mysqld.sock --pid-file=/run/mysqld/mysqld.pid
-#───────────────────────────────────────────────────────────────────────#
-# The script replaces the variables in the DB.sql file with the         #
-# values of the environment variables MYSQL_USER, MYSQL_PASSWORD,       #
-# and MYSQL_ROOT_PASSWORD. The script then executes any command-line    #
-# arguments passed to it.                                               #
-#_______________________________________________________________________#
+mysqld
