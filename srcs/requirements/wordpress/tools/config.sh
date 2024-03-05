96de1a575efb38370ea7bf33b6f8c67464152993
jlebre <josemariamlebre@gmail.com>
2024-03-05 19:29:27 +0000

    update

--- srcs/requirements/wordpress/tools/config.sh
+++ srcs/requirements/wordpress/tools/config.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 
-while mysqladmin -h"$HOSTNAME" --silent; do
-    sleep 1;
-done
+sed -i "s/___DATABASE_NAME___/$DATABASE_NAME/g" /wp-config.php;
+sed -i "s/___MYSQL_USER___/$MYSQL_USER/g" /wp-config.php;
+sed -i "s/___MYSQL_PASSWORD___/$MYSQL_PASSWORD/g" /wp-config.php;
+sed -i "s/___MYSQL_ROOT_PASSWORD___/$MYSQL_ROOT_PASSWORD/g" /wp-config.php;
+sed -i "s/___HOSTNAME___/$HOSTNAME/g" /wp-config.php;
 
 sed -i "s/listen = \/run\/php\/php7.3-fpm.sock/listen = 0.0.0.0:9000/" "/etc/php/7.3/fpm/pool.d/www.conf";
 
 chown -R www-data:www-data /var/www/*;
-mkdir -p /run/php/;
 mkdir -p /var/www/html
-cd /var/www/html;
+
 if [ "$(ls -A /var/www/html)" ]; then
     rm -rf /var/www/html/*
 fi
 
+cd /var/www/html;
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar && \
     chmod +x wp-cli.phar && \
     mv wp-cli.phar /usr/local/bin/wp
 
 wp core download --allow-root;
 
-sed -i "s/___DATABASE_NAME___/$DATABASE_NAME/g" /wp-config.php;
-sed -i "s/___MYSQL_USER___/$MYSQL_USER/g" /wp-config.php;
-sed -i "s/___MYSQL_PASSWORD___/$MYSQL_PASSWORD/g" /wp-config.php;
-sed -i "s/___MYSQL_ROOT_PASSWORD___/$MYSQL_ROOT_PASSWORD/g" /wp-config.php;
-sed -i "s/___HOSTNAME___/$HOSTNAME/g" /wp-config.php;
+while mysqladmin -h"$HOSTNAME" --silent; do
+    sleep 1;
+done
 
 mv /wp-config.php /var/www/html/wp-config.php
 wp core install --allow-root --url=$DOMAIN/ --title=$WORDPRESS_TITLE \
