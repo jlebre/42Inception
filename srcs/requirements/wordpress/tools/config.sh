d657dadba535b467d0895c400e39577a9e7a7436
jlebre <josemariamlebre@gmail.com>
2024-03-05 02:40:30 +0000

    update

--- srcs/requirements/wordpress/tools/config.sh
+++ srcs/requirements/wordpress/tools/config.sh
@@ -8,18 +8,14 @@ mkdir -p /run/php/;
 # file and set the appropriate permissions.                             #
 #_______________________________________________________________________#
 
-
-
-
-
 sed -i "s/listen = \/run\/php\/php8.2-fpm.sock/listen = 0.0.0.0:9000/" "/etc/php/7.3/fpm/pool.d/www.conf";
 #───────────────────────────────────────────────────────────────────────#
 # Modify the PHP-FPM configuration file to listen on port 9000          #
 # instead of a Unix socket.                                             #
 #_______________________________________________________________________#
 
-chown -R www-data:www-data /var/www/*;
-chown -R 755 /var/www/*;
+#chown -R www-data:www-data /var/www/*;
+#chown -R 755 /var/www/*;
 #───────────────────────────────────────────────────────────────────────#
 # Change ownership of the /var/www directory recursively to the         #
 # www-data user and group.                                              #
@@ -51,11 +47,11 @@ curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.pha
 
 wp core download --allow-root;
 mv /wp-config.php /var/www/html/
-wp config create --allow-root --dbname=${DATABASE_NAME} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASSWORD} --dbhost=${HOSTNAME}:3306 --dbcharset="utf8" --dbcollate="utf8_general_ci";
-wp core install --allow-root --url=${DOMAIN}/ --title=${WORDPRESS_TITLE} \
-	--admin_user=${MYSQL_USER} --admin_password=${MYSQL_PASSWORD} \
-	--admin_email=${WORDPRESS_ADMIN_EMAIL} --skip-email;
-wp user create --allow-root ${WORDPRESS_USER} ${WORDPRESS_EMAIL} --user_pass=${WORDPRESS_PASSWORD};
+#wp config create --allow-root --dbname=$DATABASE_NAME --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=${HOSTNAME}:3306 --dbcharset="utf8" --dbcollate="utf8_general_ci";
+wp core install --allow-root --url=$DOMAIN/ --title=$WORDPRESS_TITLE \
+	--admin_user=$WORDPRESS_USER --admin_password=$WORDPRESS_PASSWORD \
+	--admin_email=$WORDPRESS_ADMIN_EMAIL --skip-email;
+wp user create --allow-root $WP_USER $WP_EMAIL --user_pass=$WP_PASSWORD;
 wp theme install --allow-root twentytwentytwo --activate;
 
 /usr/sbin/php-fpm8.2 -F
