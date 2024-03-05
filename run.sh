5872f275e954290ddb512f20f58493ff45d8c883
jlebre <josemariamlebre@gmail.com>
2024-03-05 00:15:01 +0000

    update

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-apt-get update && apt-get upgrade
+apt-get update -y && apt-get upgrade -y
 apt-get install -y vim curl wget make git docker.io docker-compose \
   mariadb-server mariadb-client \
-  nginx openssl php7.3 php-mysqli php-fpm wget sendmail
+  nginx openssl php7.3 php-mysqli php-fpm wget
 
 #git clone https://www.github.com/jlebre/42Inception.git
 #cd 42Inception
