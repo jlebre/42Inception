03ac59fe479a2a06b0a8d5289ce6a48f7845d444
jlebre <josemariamlebre@gmail.com>
2024-03-05 20:13:38 +0000

    update

--- srcs/requirements/wordpress/tools/config.sh
+++ srcs/requirements/wordpress/tools/config.sh
@@ -1,29 +1,30 @@
 #!/bin/bash
 
-sleep 30;
-
 sed -i "s/___DATABASE_NAME___/$DATABASE_NAME/g" /wp-config.php;
 sed -i "s/___MYSQL_USER___/$MYSQL_USER/g" /wp-config.php;
 sed -i "s/___MYSQL_PASSWORD___/$MYSQL_PASSWORD/g" /wp-config.php;
 sed -i "s/___MYSQL_ROOT_PASSWORD___/$MYSQL_ROOT_PASSWORD/g" /wp-config.php;
 sed -i "s/___HOSTNAME___/$HOSTNAME/g" /wp-config.php;
 
-sed -i "s/listen = \/run\/php\/php7.3-fpm.sock/listen = 0.0.0.0:9000/" "/etc/php/7.3/fpm/pool.d/www.conf";
+sleep 30;
 
-chown -R www-data:www-data /var/www/*;
 mkdir -p /var/www/html
+cd /var/www/html;
 
 if [ "$(ls -A /var/www/html)" ]; then
     rm -rf /var/www/html/*
 fi
 
-cd /var/www/html;
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar && \
-    chmod +x wp-cli.phar && \
-    mv wp-cli.phar /usr/local/bin/wp
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+chmod +x wp-cli.phar
+mv wp-cli.phar /usr/local/bin/wp
 
-wp core download --allow-root;
-mv /wp-config.php /var/www/html/wp-config.php
+wp core download --allow-root
+
+cp /wp-config.php /var/www/html/wp-config.php
+
+sed -i "s/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/" "/etc/php/7.3/fpm/pool.d/www.conf";
+
 wp core install --allow-root --url=$DOMAIN/ --title=$WORDPRESS_TITLE \
 	--admin_user=$WORDPRESS_USER --admin_password=$WORDPRESS_PASSWORD \
 	--admin_email=$WORDPRESS_ADMIN_EMAIL --skip-email;
