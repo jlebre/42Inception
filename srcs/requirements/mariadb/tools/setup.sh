9e0d5ee691e48b92f713081370ffe4911ac8a17a
jlebre <josemariamlebre@gmail.com>
2024-03-05 18:18:02 +0000

    update

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-if [ ! -d "/var/lib/mysql/$DATABASE_NAME" ]; then
-mysql_install_db
-echo "STARTING MARIADB"
 service mariadb start
+
+if [ ! -d "/var/lib/mysql/$DATABASE_NAME" ]; then
+    mysql_install_db
+fi
+
 mysql_secure_installation << EOF
 
 n
@@ -16,19 +18,12 @@ Y
 Y
 EOF
 
-echo "CREATING DATABASE"
 mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "CREATE DATABASE IF NOT EXISTS $DATABASE_NAME;"
-echo "CREATING USER"
 mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
-echo "GRANTING PRIVILEGES"
 mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "GRANT ALL PRIVILEGES ON $DATABASE_NAME.* TO '$MYSQL_USER'@'%';"
-echo "FLUSH PRIVILEGES"
 mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "FLUSH PRIVILEGES;"
 
-sleep 5
-echo "STOPPING MARIADB"
 service mariadb stop
 fi
 
-echo "STARTING MARIADB"
-exec mysqld_safe --bind-address=0.0.0.0 --socket=/var/run/mysqld/mysqld.sock --pid-file=/var/run/mysqld/mysqld.pid
+exec mysqld --bind-address=0.0.0.0 --socket=/run/mysqld/mysqld.sock --pid-file=/run/mysqld/mysqld.pid
