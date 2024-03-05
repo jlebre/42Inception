6ba7008e356616ae25463599919856ddeabbd53a
jlebre <josemariamlebre@gmail.com>
2024-03-05 02:14:08 +0000

    update

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -7,6 +7,10 @@ echo "GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PAS
 echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';" | mariadb
 echo "FLUSH PRIVILEGES;" | mariadb
 
+while [ ! -S /run/mysqld/mysqld.sock ]; do
+    echo "Waiting for mysqld.sock to be created..."
+    sleep 3
+done
 
 service mariadb stop
 
