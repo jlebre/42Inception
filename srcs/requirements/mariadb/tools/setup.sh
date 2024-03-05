a79576647a8574e889d4a879ab2df31f14e0010b
jlebre <josemariamlebre@gmail.com>
2024-03-05 00:06:04 +0000

    update

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -5,17 +5,16 @@ if [ ! -d /run/mysqld ]; then
     chopwn mysql:mysql /run/mysqld
 fi
 
-if [ ! -d /var/lib/mysql/mysql ]; then
-    mysql_install_db --user=mysql --datadir=/var/lib/mysql
-    service mysql start
+service mysql start
 
-    mysql -u $MYSQL_ROOT -p"$MYSQL_ROOT_PASSWORD" "CREATE DATABASE IF NOT EXISTS $DATABASE_NAME;"
-    mysql -u $MYSQL_ROOT -p"$MYSQL_ROOT_PASSWORD" "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
-    mysql -u $MYSQL_ROOT -p"$MYSQL_ROOT_PASSWORD" "GRANT ALL PRIVILEGES ON $DATABASE_NAME.* TO '$MYSQL_USER'@'%';"
-    mysql -u $MYSQL_ROOT -p"$MYSQL_ROOT_PASSWORD" "FLUSH PRIVILEGES;"
-    mysql -u $MYSQL_ROOT -p"$MYSQL_ROOT_PASSWORD" "ALTER USER '$MYSQL_ROOT'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'"
-    service mysql stop
-    kill $(cat /var/run/mysqld/mysqld.pid)
-fi
+echo "CREATE DATABASE IF NOT EXISTS $DATABASE_NAME;" > DB.sql
+echo "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" >> DB.sql
+echo "GRANT ALL PRIVILEGES ON $DATABASE_NAME.* TO '$MYSQL_USER'@'%';" >> DB.sql
+echo "ALTER USER '$MYSQL_ROOT'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';" >> DB.sql
+echo "FLUSH PRIVILEGES;" >> DB.sql
+
+mysql < DB.sql
+
+kill $(cat /var/run/mysqld/mysqld.pid)
 
 mysqld
