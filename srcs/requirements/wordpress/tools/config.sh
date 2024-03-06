989cbe8056c3b2ac1e37dd67445e90563481ce0f
jlebre <josemariamlebre@gmail.com>
2024-03-06 01:47:06 +0000

    update

--- srcs/requirements/wordpress/tools/config.sh
+++ srcs/requirements/wordpress/tools/config.sh
@@ -6,9 +6,8 @@ sed -i "s/___MYSQL_PASSWORD___/$MYSQL_PASSWORD/g" /wp-config.php;
 sed -i "s/___MYSQL_ROOT_PASSWORD___/$MYSQL_ROOT_PASSWORD/g" /wp-config.php;
 sed -i "s/___HOSTNAME___/$HOSTNAME/g" /wp-config.php;
 
-sleep 10;
 mkdir -p /var/www/html
-cd /var/www/html;
+cd /var/www/html
 
 if [ "$(ls -A /var/www/html)" ]; then
     rm -rf /var/www/html/*
@@ -20,16 +19,19 @@ mv wp-cli.phar /usr/local/bin/wp
 
 wp core download --allow-root
 
-sleep 10
+sleep 15
 
 cp /wp-config.php /var/www/html/wp-config.php
-chmod -R 666 /var/www/html/
+
+chmod -R 755 /var/www/html
+chmod -R 644 /var/www/html/*
 
 sed -i "s/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/" "/etc/php/7.3/fpm/pool.d/www.conf"
 
 wp core install --allow-root --url=$DOMAIN/ --title=$WORDPRESS_TITLE \
 	--admin_user=$WORDPRESS_USER --admin_password=$WORDPRESS_PASSWORD \
 	--admin_email=$WORDPRESS_ADMIN_EMAIL --skip-email
+
 wp user create --allow-root $WP_USER $WP_EMAIL --user_pass=$WP_PASSWORD
 
 chown -R www-data:www-data /var/www/html
