a1eb975206405f6a9cd5b75c30c48d82279d5667
jlebre <josemariamlebre@gmail.com>
2024-03-06 00:42:54 +0000

    update

--- srcs/requirements/wordpress/tools/config.sh
+++ srcs/requirements/wordpress/tools/config.sh
@@ -29,8 +29,7 @@ wp core install --allow-root --url=$DOMAIN/ --title=$WORDPRESS_TITLE \
 	--admin_user=$WORDPRESS_USER --admin_password=$WORDPRESS_PASSWORD \
 	--admin_email=$WORDPRESS_ADMIN_EMAIL --skip-email
 wp user create --allow-root $WP_USER $WP_EMAIL --user_pass=$WP_PASSWORD
-wp theme install --allow-root twentytwentytwo --activate
+wp theme twentytwentytwo --activate --allow-root
 
-chmod 666 /var/www/html/wp-content
 
 /usr/sbin/php-fpm7.3 -F
