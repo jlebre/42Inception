2e735907e7626d9752742f7e5bb1c6659044d1df
jlebre <josemariamlebre@gmail.com>
2024-03-06 21:03:38 +0000

    update

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -19,7 +19,7 @@ mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "CREATE USER IF NOT EXISTS '$MYSQL_USE
 mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "GRANT ALL PRIVILEGES ON $DATABASE_NAME.* TO '$MYSQL_USER'@'%';"
 mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "FLUSH PRIVILEGES;"
 
-sleep 10
+sleep 5
 service mysql stop
 fi
 
--- srcs/requirements/wordpress/tools/config.sh
+++ srcs/requirements/wordpress/tools/config.sh
@@ -28,14 +28,14 @@ wp config create --dbname=${DATABASE_NAME} --dbuser=${MYSQL_USER} \
 	--dbpass=${MYSQL_PASSWORD} --dbhost=${HOSTNAME}:3306 \
 	--dbcharset="utf8" --dbcollate="utf8_general_ci" --allow-root
 
-wp config set AUTH_KEY ${AUTH_KEY} --type=constant
-wp config set SECURE_AUTH_KEY ${SECURE_AUTH_KEY} --type=constant
-wp config set LOGGED_IN_KEY ${LOGGED_IN_KEY} --type=constant
-wp config set NONCE_KEY ${NONCE_KEY} --type=constant
-wp config set AUTH_SALT ${AUTH_SALT} --type=constant
-wp config set SECURE_AUTH_SALT ${SECURE_AUTH_SALT} --type=constant
-wp config set LOGGED_IN_SALT ${LOGGED_IN_SALT} --type=constant
-wp config set NONCE_SALT ${NONCE_SALT} --type=constant
+wp config set AUTH_KEY ${AUTH_KEY} --type=constant --allow-root
+wp config set SECURE_AUTH_KEY ${SECURE_AUTH_KEY} --type=constant --allow-root
+wp config set LOGGED_IN_KEY ${LOGGED_IN_KEY} --type=constant --allow-root
+wp config set NONCE_KEY ${NONCE_KEY} --type=constant --allow-root
+wp config set AUTH_SALT ${AUTH_SALT} --type=constant --allow-root
+wp config set SECURE_AUTH_SALT ${SECURE_AUTH_SALT} --type=constant --allow-root
+wp config set LOGGED_IN_SALT ${LOGGED_IN_SALT} --type=constant --allow-root
+wp config set NONCE_SALT ${NONCE_SALT} --type=constant --allow-root
 
 wp core install --url=${DOMAIN} --title=${WORDPRESS_TITLE} \
 	--admin_user=${WORDPRESS_USER} --admin_password=${WORDPRESS_PASSWORD} \
