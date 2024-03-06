2c95be91bb392c9db155a098c9201a8aaedcb04d
jlebre <josemariamlebre@gmail.com>
2024-03-06 02:21:20 +0000

    update

--- srcs/requirements/wordpress/tools/wp-config.php
+++ srcs/requirements/wordpress/tools/wp-config.php
@@ -77,7 +77,7 @@ $table_prefix = 'wp_';
  *
  * @link https://wordpress.org/support/article/debugging-in-wordpress/
  */
-define( 'WP_DEBUG', false );
+define( 'WP_DEBUG', true );
 
 /* E é tudo. Pare de editar! */
 
